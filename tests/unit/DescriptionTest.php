840a9a5d5f8e30caaaf82d3b0dea995934bb0291
Guido Scialfa <dev@guidoscialfa.com>
2019-02-16 18:04:18 +0100

    Update file docblock for tests and sensitive case issue on tests directories

--- tests/unit/DescriptionTest.php
+++ tests/unit/DescriptionTest.php
@@ -1,5 +1,15 @@
 <?php # -*- coding: utf-8 -*-
-// phpcs:disable
+
+/*
+ * This file is part of the WordPress Model package.
+ *
+ * (c) Guido Scialfa <dev@guidoscialfa.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace WordPressModel\Tests\Unit;
 
--- tests/unit/Model/ArchiveHeaderTest.php
+++ tests/unit/Model/ArchiveHeaderTest.php
@@ -1,13 +1,15 @@
 <?php # -*- coding: utf-8 -*-
 
 /*
- * This file is part of the WordPress Theme Model package.
+ * This file is part of the WordPress Model package.
  *
  * (c) Guido Scialfa <dev@guidoscialfa.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace WordPressModel\Tests\Unit\Model;
 
