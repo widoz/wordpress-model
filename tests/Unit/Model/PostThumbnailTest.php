43ec6bdc0a6662dcf68051e607efd94b2e6e7f8b
Guido Scialfa <dev@guidoscialfa.com>
2019-06-29 23:37:26 +0200

    Remove block code in test to separate steps

--- tests/Unit/Model/PostThumbnailTest.php
+++ tests/Unit/Model/PostThumbnailTest.php
@@ -33,51 +33,45 @@ class PostThumbnailTest extends TestCase
      */
     public function testFilterGetAppliedWithCorrectData()
     {
-        {
-            $attachmentImageData = ['attachment_image_data'];
-            $testeePermalink = 'testee_permalink';
+        $attachmentImageData = ['attachment_image_data'];
+        $testeePermalink = 'testee_permalink';
 
-            $post = $this->getMockBuilder('\\WP_Post')->getMock();
-            $attachmentImage = $this->createMock(AttachmentImage::class);
+        $post = $this->getMockBuilder('\\WP_Post')->getMock();
+        $attachmentImage = $this->createMock(AttachmentImage::class);
 
-            $testee = $this->buildTesteeMock(
-                Testee::class,
-                [$post, $attachmentImage],
-                ['permalink', 'hasThumbnail'],
-                ''
-            )->getMock();
-        }
+        $testee = $this->buildTesteeMock(
+            Testee::class,
+            [$post, $attachmentImage],
+            ['permalink', 'hasThumbnail'],
+            ''
+        )->getMock();
 
-        {
-            $attachmentImage
-                ->expects($this->once())
-                ->method('data')
-                ->willReturn($attachmentImageData);
+        $attachmentImage
+            ->expects($this->once())
+            ->method('data')
+            ->willReturn($attachmentImageData);
 
-            $testee
-                ->expects($this->once())
-                ->method('permalink')
-                ->willReturn($testeePermalink);
+        $testee
+            ->expects($this->once())
+            ->method('permalink')
+            ->willReturn($testeePermalink);
 
-            $testee
-                ->expects($this->once())
-                ->method('hasThumbnail')
-                ->willReturn(true);
+        $testee
+            ->expects($this->once())
+            ->method('hasThumbnail')
+            ->willReturn(true);
 
-            Filters\expectApplied(Testee::FILTER_DATA)
-                ->with([
-                    'link' => [
-                        'attributes' => [
-                            'href' => $testeePermalink,
-                        ],
+        Filters\expectApplied(Testee::FILTER_DATA)
+            ->with([
+                'link' => [
+                    'attributes' => [
+                        'href' => $testeePermalink,
                     ],
-                    'attachmentImage' => $attachmentImageData,
-                ]);
-        }
+                ],
+                'attachmentImage' => $attachmentImageData,
+            ]);
 
-        {
-            $testee->data();
-        }
+        $testee->data();
     }
 
     /**
@@ -85,38 +79,30 @@ class PostThumbnailTest extends TestCase
      */
     public function testPermalink()
     {
-        {
-            $postPermalinkStub = 'post_permalink_stub';
+        $postPermalinkStub = 'post_permalink_stub';
 
-            $post = $this->getMockBuilder('WP_Post')->getMock();
-            $attachmentImage = $this->createMock(AttachmentImage::class);
-            list($testee, $testeeMethod) = $this->buildTesteeMethodMock(
-                Testee::class,
-                [$post, $attachmentImage],
-                'permalink',
-                []
-            );
-        }
+        $post = $this->getMockBuilder('WP_Post')->getMock();
+        $attachmentImage = $this->createMock(AttachmentImage::class);
+        list($testee, $testeeMethod) = $this->buildTesteeMethodMock(
+            Testee::class,
+            [$post, $attachmentImage],
+            'permalink',
+            []
+        );
 
-        {
-            Functions\expect('get_permalink')
-                ->once()
-                ->with($post)
-                ->andReturn($postPermalinkStub);
+        Functions\expect('get_permalink')
+            ->once()
+            ->with($post)
+            ->andReturn($postPermalinkStub);
 
-            Filters\expectApplied(Testee::FILTER_PERMALINK)
-                ->once()
-                ->with($postPermalinkStub)
-                ->andReturn($postPermalinkStub);
-        }
+        Filters\expectApplied(Testee::FILTER_PERMALINK)
+            ->once()
+            ->with($postPermalinkStub)
+            ->andReturn($postPermalinkStub);
 
-        {
-            $response = $testeeMethod->invoke($testee);
-        }
+        $response = $testeeMethod->invoke($testee);
 
-        {
-            self::assertSame($postPermalinkStub, $response);
-        }
+        self::assertSame($postPermalinkStub, $response);
     }
 
     /**
@@ -124,34 +110,26 @@ class PostThumbnailTest extends TestCase
      */
     public function testPostThumbnailExists()
     {
-        {
-            $post = $this->getMockBuilder('WP_Post')->getMock();
-            $attachmentImage = $this->createMock(AttachmentImage::class);
-            list($testee, $testeeMethod) = $this->buildTesteeMethodMock(
-                Testee::class,
-                [$post, $attachmentImage],
-                'hasThumbnail',
-                []
-            );
-        }
+        $post = $this->getMockBuilder('WP_Post')->getMock();
+        $attachmentImage = $this->createMock(AttachmentImage::class);
+        list($testee, $testeeMethod) = $this->buildTesteeMethodMock(
+            Testee::class,
+            [$post, $attachmentImage],
+            'hasThumbnail',
+            []
+        );
 
-        {
-            Functions\expect('current_theme_supports')
-                ->once()
-                ->with('post-thumbnails')
-                ->andReturn(true);
+        Functions\expect('current_theme_supports')
+            ->once()
+            ->with('post-thumbnails')
+            ->andReturn(true);
 
-            Functions\expect('has_post_thumbnail')
-                ->with($post)
-                ->andReturn(true);
-        }
+        Functions\expect('has_post_thumbnail')
+            ->with($post)
+            ->andReturn(true);
 
-        {
-            $response = $testeeMethod->invoke($testee);
-        }
+        $response = $testeeMethod->invoke($testee);
 
-        {
-            self::assertSame(true, $response);
-        }
+        self::assertSame(true, $response);
     }
 }
